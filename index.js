00bb25d9ea8dda7e345b3bf4ea9328acdd8a3cde
singhbraj <braj333singh@gmail.com>
2020-07-01 22:30:17 +0530

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser'); 
+const { User } = require('./models/user')
 
-mongoose.connect('mongodb+srv://braj3978:braj3978@cluster0.0yzsj.mongodb.net/test?retryWrites=true&w=majority',
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI,
 {useNewUrlParser:true, useUnifiedTopology: true}).then(()=>{
     console.log('DB connected')
 }).catch(err => console.error(err));
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+
 app.get('/',(req,res)=>{
-    res.send('hello world!!')
+    res.send('Hello Braj Kishro and Welcome!!')
 })
 
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body)
+        user.save((err,userData)=>{
+            if(err) return res.json({success:false,err})
+            return res.status(200).json({
+                success:true
+            });
+        })
+
+        
+})
 
 app.listen(5000);
