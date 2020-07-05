db65ace62d36ad3ea1929245f75f8c8bb4025d3b
singhbraj <braj333singh@gmail.com>
2020-07-05 21:31:00 +0530

    finishing with client side

--- server/index.js
+++ server/index.js
@@ -1,101 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser'); 
-const config = require('./config/key');
+const path = require("path");
+const cors = require('cors')
 
-const { User } = require('./models/user');
-const { auth } = require("./middleware/auth"); 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
+const config = require("./config/key");
 
-mongoose.connect(config.mongoURI,
-{useNewUrlParser:true, useUnifiedTopology: true}).then(()=>{
-    console.log('DB connected')
-}).catch(err => console.error(err));
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
 
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors())
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use('/api/users', require('./routes/users'));
 
 
-app.get('/',(req,res)=>{
-    res.json({"hello":"I am happy to deploy my application"})
-})
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
 
-app.get("/api/user/auth", auth, (req,res)=>{
-  res.status(200).json({
-      _id:req._id,
-      isAuth:true,
-      email:req.user.email,
-      name:req.user.name,
-      lastname:req.user.lastname,
-      role:req.user.role
-  })
-  
-})
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
 
-
-app.post('/api/users/register',(req,res)=>{
-    const user = new User(req.body)
-        user.save((err,userData)=>{
-            if(err) return res.json({success:false,err})
-            return res.status(200).json({
-                success:true
-            });
-        })        
-})
-
-
-app.post('/api/user/login',(req,res)=>{
-    // find the email
-
-    User.findOne({email:req.body.email},(err,user)=>{
-        if(!user)
-        return  res.json({
-            loginSuccess:false,
-            message:"Auth failed,email not found"
-        });
-    // compare the password
-
-    user.comparePassword(req.body.password,(err,isMatch)=>{
-        if(!isMatch){
-            return res.json({
-                loginSuccess:false,
-                message:"wrong password"
-            })
-        }
-    })
-
-    //genrerateToken
-
-    user.generateToken((err,user)=>{
-        if(err) return res.status(400).send(err);
-        res.cookie("x_auth",user.token)
-           . status(200)
-           .json({
-               loginSuccess: true
-           })
-
-    })
-    })    
-});
-
-
-app.get("/api/user/logout",auth,(req,res)=>{
-    User.findOneAndUpdate({_id:req.user._id},{
-        token:""
-    },(err,doc)=>{
-        if(err) return res.json({success:false,err})  
-        return res.status(200).send({success:true})
-    })
-})
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000
 
-app.listen(port,()=>{
-    console.log(`Server Running at ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`)
+});
